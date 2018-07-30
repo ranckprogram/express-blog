93b24f1a3ef9669660103416ddd8d67adce3c728
ranckprogram <1474574022@qq.com>
2018-07-30 18:11:50 +0800

    新增功能 => 文章的分页（同步分页）

--- router/web/components/list.js
+++ router/web/components/list.js
@@ -1,14 +1,40 @@
 // 文章列表
 let db = require('../../../lib/db')
-
+const async = require('async')
 
 module.exports = function (req, res, next) {
-	db.query(`select * from article_table`, function (err, list) {
+	let page = req.query.page || 1
+	let begin = (page - 1) * 10
+	async.parallel({
+		list: function (callback) {
+			db.query(`select * from article_table limit ${begin}, 10`, function (err, list) {
+				if (err) {
+					res.sqlError(err)
+				} else {
+					callback(null, list)
+				}
+			})
+		},
+		total: function (callback) {
+			db.query(`select count(*) from article_table`, function (err, pageCount) {
+				if (err) {
+					res.sqlError(err)
+				} else {
+					callback(null, pageCount)
+				}
+			})
+		}
+	}, function (err, results) {
 		if (err) {
-			res.sqlError(err)
+			console.error(err)
 		} else {
-			res.locals.list = list
+			res.locals.listPage = {
+				page: page,
+				total: results.total[0]['count(*)']
+			}
+			res.locals.list = results.list
 			next()
 		}
 	})
+
 }
